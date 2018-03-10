937b269f0b67deb9d6c8085a8f4ecdd41cfe3897
thongtruong23 <37130108+thongtruong23@users.noreply.github.com>
2018-03-10 08:44:14 +0700

    Update Bai110.java

--- Bai110.java
+++ Bai110.java
@@ -4,6 +4,24 @@ public class Bai110 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+	   int test = 0;
+	   Scanner in = new Scanner(System.in);	
+	   System.out.print("Input a positive integer: ");
+	   int n = in.nextInt(); 
+       if (n < 1) {
+			 System.out.print(Boolean.toString(false));
+			 test = 1;
+		}
+		if ((n & (n - 1)) != 0) {
+			System.out.print(Boolean.toString(false));
+			test = 1;
+		}
+		if (test==0)
+		{
+			System.out.print(Boolean.toString((n & 0x55555555) != 0));      	
+		}
+		System.out.print("\n")
 
 	}
 
