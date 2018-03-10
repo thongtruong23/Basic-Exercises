c1e6ba687df76a4844bbfc3b4de9cc15da67c70e
thongtruong23 <37130108+thongtruong23@users.noreply.github.com>
2018-03-10 09:11:43 +0700

    Update Bai22.java

--- Bai22.java
+++ Bai22.java
@@ -4,6 +4,20 @@ public class Bai22 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+  Scanner sc = new Scanner(System.in);
+  long binaryNumber, decimalNumber = 0, j = 1, remainder;
+  System.out.print("Input a binary number: ");
+  binaryNumber = sc.nextLong();
+
+  while (binaryNumber != 0) 
+  {
+   remainder = binaryNumber % 10;
+   decimalNumber = decimalNumber + remainder * j;
+   j = j * 2;
+   binaryNumber = binaryNumber / 10;
+  }
+  System.out.println("Decimal Number: " + decimalNumber);
 
 	}
 
