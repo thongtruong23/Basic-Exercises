99cf99186aca1ffae0fc7aa56992903adb151102
thongtruong23 <37130108+thongtruong23@users.noreply.github.com>
2018-03-10 08:35:25 +0700

    Update Bai102.java

--- Bai102.java
+++ Bai102.java
@@ -4,6 +4,23 @@ public class Bai102 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+	int[] array_nums = {11, 11, 13, 31, 45, 20, 33, 53};
+    int result = 1; 
+	System.out.println("Original Array: "+Arrays.toString(array_nums)); 
+	
+	for(int i = 0; i < array_nums.length; i++)
+     {
+      	if(array_nums[i] == 10 || array_nums[i] == 30)
+		{	
+  		
+		   result =0;
+		}
+      }
+	 if (result==0)
+ 	   System.out.printf( String.valueOf(true));	         
+     else
+	   System.out.printf(String.valueOf(false));
 
 	}
 
