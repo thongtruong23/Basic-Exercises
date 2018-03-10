624c8ceadf0b2a416136f4123e9657116ec1f572
thongtruong23 <37130108+thongtruong23@users.noreply.github.com>
2018-03-10 08:45:31 +0700

    Update Bai109.java

--- Bai109.java
+++ Bai109.java
@@ -4,6 +4,13 @@ public class Bai109 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 Scanner in = new Scanner(System.in);	
+       System.out.print("Input a positive integer: ");
+        int n = in.nextInt(); 
+		if (n>0)
+		{
+           System.out.println("Number of rows: "+((int)((Math.sqrt(8 * (long)n + 1) - 1) / 2)));	
+	
 
 	}
 
