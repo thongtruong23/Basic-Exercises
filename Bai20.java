b5827a25550febdfd98986c6367e0d4687488db6
thongtruong23 <37130108+thongtruong23@users.noreply.github.com>
2018-03-10 09:08:06 +0700

    Update Bai20.java

--- Bai20.java
+++ Bai20.java
@@ -4,6 +4,26 @@ public class Bai20 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+        int dec_num, rem;
+        String hexdec_num="";
+        
+        /* hexadecimal number digits */
+        
+        char hex[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        
+        Scanner in = new Scanner(System.in);
+		
+        System.out.print("Input a decimal number: ");
+        dec_num = in.nextInt();
+		
+        while(dec_num>0)
+        {
+            rem = dec_num%16;
+            hexdec_num = hex[rem] + hexdec_num;
+            dec_num = dec_num/16;
+        }
+        System.out.print("Hexadecimal number is : "+hexdec_num+"\n");     
 
 	}
 
