91173b2e370960755b3b223eea010bfc6eb6e68f
thongtruong23 <37130108+thongtruong23@users.noreply.github.com>
2018-03-10 08:39:33 +0700

    Update Bai114.java

--- Bai114.java
+++ Bai114.java
@@ -4,6 +4,25 @@ public class Bai114 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+	 String str = "abcdef"; 
+     char[] A = str.toCharArray();
+	 int offset=3;
+     int len = A.length;
+     offset %= len;
+     reverse(A, 0, len - offset - 1);
+     reverse(A, len - offset, len - 1);
+     reverse(A, 0, len - 1);
+     System.out.println("\n"+Arrays.toString(A));
+    }
+
+    private static void reverse(char[] str, int start, int end) {
+        while (start < end) {
+            char temp = str[start];
+            str[start] = str[end];
+            str[end] = temp;
+            start++;
+            end--;
 
 	}
 
