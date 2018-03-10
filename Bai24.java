3837ca2a6665876df90f2f03ba27b966335505d7
thongtruong23 <37130108+thongtruong23@users.noreply.github.com>
2018-03-10 09:14:46 +0700

    Update Bai24.java

--- Bai24.java
+++ Bai24.java
@@ -4,7 +4,39 @@ public class Bai24 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	
+        int binnum, binnum1,rem, decnum=0, quot, i=1, j;
+        int octnum[] = new int[100];
+        Scanner scan = new Scanner(System.in);
+		
+        System.out.print("Enter Binary Number : ");
+        binnum = scan.nextInt();
+        binnum1=binnum;
+     
+      while(binnum > 0)
+        {
+            rem = binnum % 10;
+            decnum = decnum + rem*i;
+            //System.out.println(rem);
+            i = i*2;
+            binnum = binnum/10;
+        }   
 
+		i=1;
+        quot = decnum;
+		
+        while(quot > 0)
+        {
+            octnum[i++] = quot % 8;
+            quot = quot / 8;
+        }
+		
+        System.out.print("Equivalent Octal Value of " +binnum1+ " is :");
+        for(j=i-1; j>0; j--)
+        {
+            System.out.print(octnum[j]);
+        }
+ System.out.print("\n");
 	}
 
 }
