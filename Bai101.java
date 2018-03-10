0dc93e2110bd3c4c24964ff0a9045e208c08dfa2
thongtruong23 <37130108+thongtruong23@users.noreply.github.com>
2018-03-10 08:42:01 +0700

    Update Bai101.java

--- Bai101.java
+++ Bai101.java
@@ -4,7 +4,23 @@ public class Bai101 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	
+	int[] array_nums = {10, 11, 10, 30, 45, 20, 33, 53};
+    int result = 0; 
+	System.out.println("Original Array: "+Arrays.toString(array_nums)); 
+    
+	int ctr1 = 0;
+    int ctr2 = 0;
+      
+    for(int i = 0; i < array_nums.length; i++) {
+        if(array_nums[i] == 10)
+            ctr1++;
+                        
+        if(array_nums[i] == 20)
+            ctr2++;
+    }                                      
+    System.out.printf(String.valueOf(ctr1 > ctr2));
+	System.out.printf("\n");
 	}
 
 }
